e1c0177af1fa72c0740dbb1216bc389333076e54
Polly Hsu <pollylearnhsu@gmail.com>
2025-02-26 16:42:13 +0800

    修改：layouts 排版

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,26 +6,27 @@
 
 
 @section('content')
-<div class="mt-[60px]">
+<div class="mt-[200px]">
     <!-- banner 輪播圖 -->
-    <section class="max-w-full h-[736px] border-b-2 shadow-[0_15px_0_0_brandGray-normalLight] flex flex-col">
-        <div class="relative w-full h-[600px] overflow-hidden flex justify-center items-start mb-10">
+    <section class="max-w-full h-[736px] overflow-hidden border-b-2 shadow-[0_15px_0_0_brandGray-normalLight] flex flex-col">
+        <div class="relative w-full h-[600px] overflow-hidden flex justify-center items-start gap-5 mb-10">
             <!-- 左側圖片 -->
-            <div class="absolute left-[-400px] w-[720px] h-[600px]" style="margin-right: 20px;">
-                <img src="{{ asset('images/home_test1.JPG') }}" alt="" class="w-full h-full object-cover">
+            <div class="w-[720px] h-[600px] flex-shrink-0">
+                <a href=""><img src="{{ asset('images/home_test1.JPG') }}" alt="" class="w-full h-full object-cover"></a>
             </div>
             <!-- 中間圖片 -->
-            <div class="relative w-[720px] h-[600px] z-10">
-                <img src="{{ asset('images/home_test2.JPG') }}" alt="" class="w-full h-full object-cover">
+            <div class="w-[720px] h-[600px] flex-shrink-0 z-10">
+                <a href=""><img src="{{ asset('images/home_test2.JPG') }}" alt="" class="w-full h-full object-cover"></a>
             </div>
             <!-- 右側圖片 -->
-            <div class="absolute right-[-400px] w-[720px] h-[600px]" style="margin-left: 20px;">
-                <img src="{{ asset('images/home_test3.JPG') }}" alt="" class="w-full h-full object-cover">
+            <div class="w-[720px] h-[600px] flex-shrink-0">
+                <a href=""><img src="{{ asset('images/home_test3.JPG') }}" alt="" class="w-full h-full object-cover"></a>
             </div>
         </div>
 
+
         <!-- progress + btn -->
-        <div class="w-full h-[50px] flex justify-senter items-center px-[100px]">
+        <div class="max-w-[1440px] h-[50px] flex justify-senter items-center px-[100px]">
             <div class="flex justify-center items-center me-[300px]">
                 <div class="step1 w-[120px] h-[1px] bg-brandRed-normal"></div>
                 <div class="step1 w-[12px] h-[12px] bg-brandRed-normal rounded-full"></div>
@@ -80,7 +81,7 @@
                             <div class="w-[50px] h-[50px] flex justify-center items-center">M</div>
                             <div class="w-[50px] h-[50px] flex justify-center items-center">S</div>
                         </div>
-                        <button type="button" class="w-[300px] h-[58px] text-[24px] text-semibold text-brandGray-lightLight flex justify-center items-center bg-brandRed-normal rounded-md">直接購買<span class="w-[24px] h-[24px] text-brandGray-lightLight icon-[ep--arrow-right-bold]"></span></button>
+                        <button type="button" class="w-[300px] h-[58px] text-[24px] text-semibold text-brandGray-lightLight flex justify-center items-center bg-brandRed-normal rounded-md hover:bg-brandRed-lightActive">直接購買&nbsp;<span class="w-[24px] h-[24px] text-brandGray-lightLight icon-[ep--arrow-right-bold]"></span></button>
                     </div>
                 </div>
             </div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav class="text-brandGray-normal font-lexend font-normal">
+<nav class="fixed top-[45px] z-50 w-full bg-white text-brandGray-normal font-lexend font-normal">
     <div class="flex w-full h-[94px] lg:px-[85px] md:px-[60px] px-[20px] py-5 justify-between items-center border-b border-b-normalLight md:border-0 lg:border-b lg:border-b-normalLight">
         <!-- 左側區塊 -->
         <div class="leftArea flex justify-start items-center gap-10">
@@ -7,16 +7,16 @@
                 <img src="{{ asset('images/logo-nav1.png') }}" class="h-8 me-[12px]" alt="">
                 <img src="{{ asset('images/logo-nav2.png') }}" class="h-5" alt="">
             </a>
-            <div class="text-lg prodsBtn hidden lg:block cursor-pointer">商品<span>＋</span></div>
-            <a href="" class="text-lg hidden lg:block">關於我們</a>
+            <div class="text-lg prodsBtn hidden lg:block cursor-pointer">商品&nbsp;<span class="plusIcon">＋</span></div>
+            <a href="" class="text-lg hidden lg:block hover:text-brandGray-normalLight active:text-brandGray-lightHover " >關於我們</a>
         </div>
         <!-- 右側區塊 -->
         <div class="rightArea flex justify-end items-center gap-10">
             <!-- 搜尋框 -->
             <div class="hidden md:block">
                 <div class="relative flex items-center">
-                    <input type="text" class="flex w-[254px] h-[30px] px-4 py-3 items-center gap-2 rounded-[20px] border border-[#484848] bg-white">
-                    <span class="absolute left-5 w-[16px] h-[16px] icon-[basil--search-outline]"></span>
+                    <input type="text" placeholder="搜尋商品" class="ps-10 flex w-[254px] h-[30px] px-4 py-3 items-center gap-2 rounded-[20px] border border-[#484848] bg-white">
+                    <span class="absolute top-2 left-4 w-[16px] h-[16px] icon-[basil--search-outline]"></span>
                 </div>
             </div>
 
@@ -24,20 +24,20 @@
             <!-- wishList -->
             <div class="flex justify-center items-center gap-[16px] md:gap-10">
                 <a href="" class="flex items-center justify-center">
-                    <span class="w-[20px] h-[20px] md:w-[25px] md:h-[25px] icon-[mynaui--heart]"></span>
+                    <span class="w-[20px] h-[20px] md:w-[25px] md:h-[25px] hover:text-brandGray-normalLight active:text-brandGray-lightHover icon-[mynaui--heart]"></span>
                 </a>
                 <!-- cart -->
                 <a href="" class="flex items-center justify-center">
-                    <span class="w-[20px] h-[20px] md:w-[25px] md:h-[25px] icon-[iconoir--cart]"></span>
+                    <span class="w-[20px] h-[20px] md:w-[25px] md:h-[25px] hover:text-brandGray-normalLight active:text-brandGray-lightHover icon-[iconoir--cart]"></span>
                 </a>
                 <!-- account -->
                 <a href="" class="flex items-center justify-center">
-                    <span class="w-[20px] h-[20px] md:w-[25px] md:h-[25px] icon-[qlementine-icons--user-16]"></span>
+                    <span class="w-[20px] h-[20px] md:w-[25px] md:h-[25px] hover:text-brandGray-normalLight active:text-brandGray-lightHover icon-[qlementine-icons--user-16]"></span>
                 </a>
                 <!-- hamMenu -->
                 <div id="hamBtn" class="block md:hidden cursor-pointer">
                     <div class="flex items-center justify-center">
-                        <span class="w-[20px] h-[20px] md:w-[25px] md:h-[25px] icon-[cil--hamburger-menu]"></span>
+                        <span class="w-[20px] h-[20px] md:w-[25px] md:h-[25px] hover:text-brandGray-normalLight active:text-brandGray-lightHover icon-[cil--hamburger-menu]"></span>
                     </div>
                 </div>
             </div>
@@ -45,42 +45,42 @@
     </div>
 
     <!-- 平板版 變版 -->
-    <div class="hidden md:block lg:hidden h-[50px] px-[60px] border-b border-b-normalLight">
-        <span class="prodsBtn me-10 cursor-pointer">商品<span>＋</span></span>
-        <a href="">關於我們</a>
+    <div class="hidden text-lg md:block lg:hidden h-[50px] px-[60px] border-b border-b-normalLight">
+        <span class="prodsBtn me-10 cursor-pointer">商品&nbsp;<span class="plusIcon">＋</span></span>
+        <a href="" class="hover:text-brandGray-normalLight active:text-brandGray-lightHover">關於我們</a>
     </div>
 
     <!-- 電腦＋平板版 下拉式選單 -->
     <div id="dropDownMenuPc" class="z-30 bg-white absolute hidden w-full h-[62px] border-b border-b-normalLight">
         <div class="h-full lg:px-[85px] md:px-[60px] flex justify-start items-center gap-[30px] py-1">
-            <a href="{{ route('categories_accessories') }}">飾品 Accessories</a>
-            <a href="{{ route('categories_clothes') }}">服飾 Clothes</a>
-            <a href="">銀黏土課程 Lessons</a>
+            <a href="{{ route('categories_accessories') }}" class="hover:text-brandGray-normalLight active:text-brandGray-lightHover">飾品 Accessories</a>
+            <a href="{{ route('categories_clothes') }}" class="hover:text-brandGray-normalLight active:text-brandGray-lightHover">服飾 Clothes</a>
+            <a href="" class="hover:text-brandGray-normalLight active:text-brandGray-lightHover">銀黏土課程 Lessons</a>
         </div>
     </div>
 
     <!-- 手機版 下拉式選單 -->
-    <div id="dropDownMenuPhone" class="absolute w-full hidden bg-white h-[96px] px-[20px] border-b border-b-normalLight">
+    <div id="dropDownMenuPhone" class="z-30 absolute w-full hidden md:invisible bg-white h-[96px] px-[20px] border-b border-b-normalLight">
         <div class="w-full h-full flex flex-col justify-center items-center py-[12px]">
             <div class="md:hidden w-[350px] h-[30px]  mb-[12px]">
                 <div class="relative flex justify-center items-center">
-                    <input type="text" class="flex w-[350px] h-[30px] px-4 py-3 items-center gap-2 rounded-[20px] border border-[#484848] bg-white">
+                    <input type="text" placeholder="搜尋商品" class="flex w-[350px] h-[30px] px-4 ps-10 py-3 items-center gap-2 rounded-[20px] border border-[#484848] bg-white">
                     <span class="absolute top-2 left-4 w-[16px] h-[16px] icon-[basil--search-outline]"></span>
                 </div>
             </div>
             <div class="w-[350px] h-[30px] flex justify-center gap-[30px]">
-                <a href="">商品<span>＋</span></a>
-                <a href="">關於我們</a>
+                <a href="" id="prodsBtnPhone" class="hover:text-brandGray-normalLight active:text-brandGray-lightHover">商品&nbsp;<span class="plusIcon">＋</span></a>
+                <a href="" class="hover:text-brandGray-normalLight active:text-brandGray-lightHover">關於我們</a>
             </div>
         </div>
     </div>
 
     <!-- 手機版 下拉選單 子選單-->
-    <div id="dropDownMenuPhoneChild" class="absolute bg-white top-[235px] hidden w-full h-[38px] border-b border-b-normalLight">
+    <div id="dropDownMenuPhoneChild" class="z-30 absolute bg-white top-[190px] hidden w-full h-[38px] border-b border-b-normalLight">
         <div class="h-full flex justify-center items-center gap-[30px] py-1">
-            <a href="{{ route('categories_accessories') }}">飾品</a>
-            <a href="{{ route('categories_clothes') }}">服飾</a>
-            <a href="">銀黏土課程</a>
+            <a href="{{ route('categories_accessories') }}" class="hover:text-brandGray-normalLight active:text-brandGray-lightHover">飾品</a>
+            <a href="{{ route('categories_clothes') }}" class="hover:text-brandGray-normalLight active:text-brandGray-lightHover">服飾</a>
+            <a href="" class="hover:text-brandGray-normalLight active:text-brandGray-lightHover">銀黏土課程</a>
         </div>
     </div>
 
@@ -99,9 +99,11 @@
     $(document).ready(function() {
         let hideTimeout;
 
-        // 下拉式選單 顯示／隱藏
+        // 下拉選單 電腦＋平板版
         $('.prodsBtn').on('mouseenter', function() {
             clearTimeout(hideTimeout); // 防止提前隱藏
+            $(this).addClass("hover:text-brandGray-normalLight", "active:text-brandGray-lightHover");
+            $(".plusIcon").html("－");
             $("#dropDownMenuPc").removeClass("hidden");
         });
 
@@ -111,6 +113,7 @@
 
         $('.prodsBtn, #dropDownMenuPc').on('mouseleave', function() {
             hideTimeout = setTimeout(function() {
+                $(".plusIcon").html("＋");
                 $("#dropDownMenuPc").addClass("hidden");
             }, 100);
         });
@@ -131,7 +134,9 @@
 
         let hideTimeoutPhone;
 
-        $("#dropDownMenuPhone").on('mouseenter', () => {
+        // 下拉選單 手機版
+        $("#prodsBtnPhone").on('mouseenter', () => {
+            $(".plusIcon").html("－");
             clearTimeout(hideTimeoutPhone);
             $("#dropDownMenuPhoneChild").removeClass("hidden")
         })
@@ -142,6 +147,7 @@
 
         $("#dropDownMenuPhone, #dropDownMenuPhoneChild").on('mouseleave', () => {
             hideTimeoutPhone = setTimeout(() => {
+                $(".plusIcon").html("＋");
                 $("#dropDownMenuPhoneChild").addClass("hidden")
             }, 100);
         })
