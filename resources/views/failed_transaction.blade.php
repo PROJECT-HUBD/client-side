4dd3aa2e4c835d8b6e1ece578415fe1b927c4b5c
leemengju <emy0526@gmail.com>
2025-03-20 09:01:47 +0800

    brandGray

--- resources/views/failed_transaction.blade.php
+++ resources/views/failed_transaction.blade.php
@@ -11,7 +11,7 @@
 
 <div class="flex flex-col items-center justify-center w-full  min-h-full gap-5">
     <img class="h-[260px]" src="{{ asset('images/failed_transaction/fail.png')   }}" alt="">
-    <p class="text-[36px] text-brandGrey-normal font-lexend font-semibold">交易失敗</p>
+    <p class="text-[36px] text-brandGray-normal font-lexend font-semibold">交易失敗</p>
     <p class="text-[24px] text-brandGrey-normal font-normal">付款未成功，請重新下訂單</p>
     <a href="{{ route('cart') }}"
         class="flex overflow-hidden items-center py-4  font-bold text-white bg-red-500 rounded-md min-h-[56px] w-[176px] ">
